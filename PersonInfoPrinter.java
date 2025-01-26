068abb19c65817456de15adf030dc1d42e300bf7
Hannah-meidinger <hannah.meidinger@csuglobal.edu>
2025-01-26 12:57:11 -0700

    add file

---
+++ PersonInfoPrinter.java
@@ -0,0 +1,23 @@
+
+public class PersonInfoPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String firstName = "John";
+        String lastName = "Smith";
+        String streetAddress = "120 Washington Street";
+        String city = "Thornton";
+        String zipCode = "80229";
+
+        // Print the information
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+        System.out.println(zipCode);
+        
+        //RANDOM COMMENT FOR NO REASON
+
+	}
+
+}
